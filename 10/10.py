e9d6e28127c869856f5523eee81f072bb6b00d78
graham <grahampicard@gmail.com>
2022-12-10 01:03:44 -0500

    day 10 part 2

--- 10/10.py
+++ 10/10.py
@@ -21,15 +21,27 @@ def part_1(text: str, cycles: list) -> int:
     return sum(results)
 
 
-def part_2(text: str, cycles: list) -> int:
+def part_2(text: str) -> int:
     instructions = text.split("\n")
     cycle = 0
     buffer = [0]
     X = 1
     results = []
+    grid = []
+    pixels = ""
+    sprite_position = [1, 2, 3]
     while buffer:
         X += buffer.pop(0)
+        sprite_position = [X, X + 1, X + 2]
         cycle += 1
+        if len(pixels) == 40:
+            grid.append(pixels)
+            pixels = ""
+        if cycle % 40 in sprite_position:
+            pixels += "#"
+        else:
+            pixels += "."
+
         if instructions:
             current_instruction = instructions.pop(0)
             if current_instruction == "noop":
@@ -38,11 +50,8 @@ def part_2(text: str, cycles: list) -> int:
                 new_val = int(current_instruction.split(" ")[1])
                 buffer.append(0)
                 buffer.append(new_val)
-        if cycle in cycles:
-            cycles.remove(cycle)
-            results.append(X * cycle)
 
-    return sum(results)
+    return "\n".join(grid)
 
 
 def main():
@@ -53,10 +62,18 @@ def main():
     with open("10_test2.txt", "r") as f:
         data = f.read()
         assert part_1(data, [20, 60, 100, 140, 180, 220]) == 13140
+        # assert part_2(data) == 
+        a = part_2(data)
+        b = """##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######....."""
 
     with open("10_input.txt", "r") as f:
         data = f.read()
         print(part_1(data, [20, 60, 100, 140, 180, 220]))
-
+        print(part_2(data))
 
 main()
